370a61117df51c6e2cd80d1fa6488f6bfa52cfb2
racecraftr <29521802+racecraftr@users.noreply.github.com>
2022-08-11 10:28:16 -0500

    Created files.

---
+++ HelloWorldJava/HelloWorld.java
@@ -0,0 +1,7 @@
+package HelloWorldJava;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Hello, world! 
+Two words. Every language.  
+This repo is dedicated to writing "Hello, world!" in every language.
